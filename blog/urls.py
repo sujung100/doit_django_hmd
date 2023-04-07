a67362bfa76f26367587024f139976d364999331
sujung100 <myrecord100@gmail.com>
2023-04-07 14:49:58 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 urlpatterns = [
     # path('', views.index),
-    path('<int:pk>/', views.PostDetail.as_view()),
-    path('', views.PostList.as_view(), name='index'),
     # path('<int:pk>/', views.single_post_page),
+
+    # /blog/
+    path('', views.PostList.as_view(), name='post_list'),
+    # /blog/1/
+    path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+
+
+    path('category/<str:slug>/', views.category_page, name='category_filter'),
+    # /blog/category/{self.slug}/
+    # /blog/category/파이썬
+
 ]
