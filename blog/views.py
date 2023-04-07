45211d66537a96aacc271eb7bc2f66e674570115
sujung100 <myrecord100@gmail.com>
2023-04-07 11:58:37 +0900

    post 목록 페이지 카테고리 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 # def index(request):
@@ -20,6 +20,14 @@ class PostDetail(DetailView):
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        # Post 테이블에서  category 필드를 선택안한 포스트의 갯수
+        context['no_category_post_count'] = Post.objects.filter(category=None).count
+        return context
+
     # post_list.html : class이름_list.html내부적으로 정의가 되어있기 때문에 생략가능
     # 파일명을 위에있는 규칙으로 하지않을경우 명시해줘야함
     # template_name = 'blog/post_list.html'
