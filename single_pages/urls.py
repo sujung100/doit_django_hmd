ea102d1611c713b4ed5c16a18c9c5942f9a6c5aa
sujung100 <myrecord100@gmail.com>
2023-04-10 17:39:26 +0900

    navbar a태그링크 수정

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about_me/', views.about_me),
-    path('', views.landing),
+    path('', views.landing, name='index'),
+    path('about_me/', views.about_me, name='about_me'),
 ]
